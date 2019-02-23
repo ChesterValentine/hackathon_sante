a49c9e5efe9831cc33c39d030d0822a011db9039
Chester Valentine <lguesdon.pro@laposte.net>
2019-02-23 19:07:25 +0100

    Calc in ajax

--- algoCardio.php
+++ algoCardio.php
@@ -1,197 +1,100 @@
-<php
+<?php
 
-$define(FACTEUR_RISQUE_OMEGANEMIE,0.33,true); //modifier la valeur du milieu
-$define(FACTEUR_RISQUE_UBIOTE,0.33,true); //modifier la valeur du milieu
+define('FACTEUR_RISQUE_OMEGANEMIE',0.33); //modifier la valeur du milieu
+define('FACTEUR_RISQUE_UBIOTE',0.33);//modifier la valeur du milieu
 
-
-/*ces 3 variables forment l'omeganemie
-si ces trois valeurs sont bonnes l'omeganemie est égale à 0
-si une seule de ces valeurs est mauvaise l'omeganemie est égale à 1
-*/
-$RatioO6O3;//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true 
-$IndexO3;//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
-$AA_FPA;//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
-$omegapositif = 0;
-
-//test pour savoir si l'on prend en compte le facteur d'omeganemie dans le calcul
-if($RatioO6O3 >= 5)
-{
-    $omegapositif++;
-}
-if($IndexO3 < 6 || $IndexO3 > 8)
-{
-    $omegapositif++;
-}
-if(AA_FPA < 15)
-{
-    $omegapositif++;
-}
-
-$omeganemie = 0;
-if($omegapositif > 0)
-{
-  $omeganemie = 1;
-}
-
-
-
-/*ces 3 variables forment les Ubiotes
-si ces trois valeurs sont bonnes les Ubiotes est égale à 0
-si une seule de ces valeurs est mauvaise les Ubiotes est égale à 1
-*/
-$UbioteFB;//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
-$UbiotteDiversite;//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
-$UbioteRichesse;//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
-$UbiotesPositif = 0;
-
-//test pour savoir si l'on prend en compte le facteur d'omeganemie dans le calcul
-if($UbioteFB) //info à recup
-{
-  $UbiotesPositif++;
-}
-if($UbiotteDiversite) // info à recup
-{
-  $UbiotesPositif++;
-}
-if(UbioteRichesse) // info à recup
-{
-  $UbiotesPositif++;
-}
-
-$Ubiotes = 0;
-if($$UbiotesPositif > 0)
-{
-  $Ubiotes = 1;
-}
-
-
-
-boolean bHomme;
-boolean bFemme;
-boolean bTraitement = $_POST('hypertension');
-boolean bFumeur = $_POST('fumeur');
-float fTotalChol ;
-float fHDLChol ;
-int iPressionSang ;
-
-int iAge = $_POST('age');
-float fTaille = $_POST('taille');
-
-float fImc = fPoids/pow(fTaille, 2);
-
-if('homme'){
-    float micro = 52.0096 * log(iAge) + 20.0141 * log(fTotalChol) - 0.906 * log(fHDLChol) + 1.305784 * log(iPressionSang)
-    + 0.241549 * (int)bTraitement) + 12.096316 * (int)bFumeur -(4.605038 * log(iAge) * log(fTotalChol)) -(2.84367 * log(iAge) * (int)bFumeur)
-    - (2.93323 * pow(iAge, 2)) - 172.300168 ;
-
-    p = 1 - pow(0.9402, exp(micro));
-}
-else{
-    float micro = 31.764<php
-
-$define(FACTEUR_RISQUE_OMEGANEMIE,0.33,true); //modifier la valeur du milieu
-$define(FACTEUR_RISQUE_UBIOTE,0.33,true); //modifier la valeur du milieu
+define('MAX_UBIOTEFB', 5);
+define('MAX_UBIOTEDIVERSITE', 5);
+define('MAX_UBIOTERICHESSE', 5);
 
 
 /*ces 3 variables forment l'omeganemie
 si ces trois valeurs sont bonnes l'omeganemie est égale à 0
 si une seule de ces valeurs est mauvaise l'omeganemie est égale à 1
 */
-$RatioO6O3;//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true 
-$IndexO3;//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
-$AA_FPA;//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
-$omegapositif = 0;
+$RatioO6O3 = $_POST['ratio_o6o3'];//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true 
+$IndexO3 = $_POST['index_o3'];//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
+$AA_EPA = $_POST['aa_fpa'];//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
+$iOmeganemie = 0;
 
 //test pour savoir si l'on prend en compte le facteur d'omeganemie dans le calcul
 if($RatioO6O3 >= 5)
 {
-    $omegapositif++;
+  $iOmeganemie = 1;
 }
 if($IndexO3 < 6 || $IndexO3 > 8)
 {
-    $omegapositif++;
+  $iOmeganemie = 1;
 }
-if(AA_FPA < 15)
+if($AA_EPA < 15)
 {
-    $omegapositif++;
+  $iOmeganemie = 1;
 }
-
-$omeganemie = 0;
-if($omegapositif > 0)
-{
-  $omeganemie = 1;
-}
-
-
 
 /*ces 3 variables forment les Ubiotes
 si ces trois valeurs sont bonnes les Ubiotes est égale à 0
 si une seule de ces valeurs est mauvaise les Ubiotes est égale à 1
 */
-$UbioteFB;//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
-$UbiotteDiversite;//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
-$UbioteRichesse;//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
-$UbiotesPositif = 0;
+$UbioteFB = $_POST['ubiote_fb'];//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
+$UbiotteDiversite = $_POST['ubiote_diversite'];//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
+$UbioteRichesse = $_POST['ubiote_richesse'];//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
+$iUbiotes = 0;
 
 //test pour savoir si l'on prend en compte le facteur d'omeganemie dans le calcul
-if($UbioteFB) //info à recup
+if($UbioteFB > MAX_UBIOTEFB) //info à recup
 {
-  $UbiotesPositif++;
+  $iUbiotes = 1;
 }
-if($UbiotteDiversite) // info à recup
+if($UbiotteDiversite > MAX_UBIOTEDIVERSITE) // info à recup
 {
-  $UbiotesPositif++;
+  $iUbiotes = 1;
 }
-if(UbioteRichesse) // info à recup
+if(UbioteRichesse > MAX_UBIOTERICHESSE) // info à recup
 {
-  $UbiotesPositif++;
+  $iUbiotes = 1;
 }
 
-$Ubiotes = 0;
-if($$UbiotesPositif > 0)
+//Ration Tout de taille/taille
+$iHeight = $_POST['height'];
+$iWaist = $_POST['waist']
+$fRatioWaistHeight = $iWaist/$iHeight
+
+$bSex = $_POST['sex'];
+$bTreated = $_POST['treated'];
+$bSmoker = $_POST['smoker'];
+$fTotalChol $_POST['total_cholesterol'];
+$fHDLChol = $_POST['hdl_cholesterol'];
+$fSystolicBp = $_POST['systolic_bp'];
+
+$iAge = $_POST['age'];
+$iHeight = $_POST['height'];
+$iWaist = $_POST['waist']
+
+
+if($bSex)
 {
-  $Ubiotes = 1;
+  $fMicro = 52.0096 * log($iAge) + 20.0141 * log($fTotalChol) 
+  - 0.906 * log($fHDLChol) + 1.305784 * log($fSystolicBp)
+  + 0.241549 * (int)$bTreated + 12.096316 * (int)$bSmoker 
+  - (4.605038 * log($iAge) * log($fTotalChol)) -(2.84367 * log($iAge) * (int)$bSmoker)
+  - (2.93323 * pow($iAge, 2)) - 172.300168 + (int)$iOmeganemie * FACTEUR_RISQUE_OMEGANEMIE
+  + (int)$iUbiotes * FACTEUR_RISQUE_UBIOTE + $fRatioWaistHeight;
+
+  $p = 1 - pow(0.9402, exp($fMicro));
+}
+else
+{
+  $fMicro = 31.764 * log($iAge) + 22.4652 * log($fTotalChol) 
+  - 1.1877 * log($fHDLChol) + 2.5529 * log($fSystolicBp)
+  + 0.420251 * (int)$bTreated + 13.07543 * (int)$bSmoker 
+  - (5.060998 * log($iAge) * log($fTotalChol)) 
+  - (2.996945 * log($iAge) * (int)$bSmoker) - 146.5933061 
+  + (int)$iOmeganemie * FACTEUR_RISQUE_OMEGANEMIE + (int)$iUbiotes * FACTEUR_RISQUE_UBIOTE
+  + $fRatioWaistHeight;
+
+  $p = 1 - pow(0.98767, exp($fMicro));
 }
 
-
-
-boolean bHomme;
-boolean bFemme;
-boolean bTraitement = $_POST('hypertension');
-boolean bFumeur = $_POST('fumeur');
-float fTotalChol ;
-float fHDLChol ;
-int iPressionSang ;
-
-int iAge = $_POST('age');
-float fTaille = $_POST('taille');
-
-float fImc = fPoids/pow(fTaille, 2);
-
-if('homme'){
-    float micro = 52.0096 * log(iAge) + 20.0141 * log(fTotalChol) - 0.906 * log(fHDLChol) + 1.305784 * log(iPressionSang)
-    + 0.241549 * (int)bTraitement) + 12.096316 * (int)bFumeur -(4.605038 * log(iAge) * log(fTotalChol)) -(2.84367 * log(iAge) * (int)bFumeur)
-    - (2.93323 * pow(iAge, 2)) - 172.300168 ;
-
-    p = 1 - pow(0.9402, exp(micro));
-}
-else{
-    float micro = 31.764 * log(iAge) + 22.4652 * log(fTotalChol) - 1.1877 * log(fHDLChol) + 2.5529 * log(iPressionSang)
-    + 0.420251 * (int)bTraitement) + 13.07543 * (int)bFumeur -(5.060998 * log(iAge) * log(fTotalChol)) -(2.996945 * log(iAge) * (int)bFumeur)
-  - 146.5933061 ;
-
-    p = 1 - pow(0.98767, exp(micro));
-}
-
-return p;
-
-?>* log(iAge) + 22.4652 * log(fTotalChol) - 1.1877 * log(fHDLChol) + 2.5529 * log(iPressionSang)
-    + 0.420251 * (int)bTraitement) + 13.07543 * (int)bFumeur -(5.060998 * log(iAge) * log(fTotalChol)) -(2.996945 * log(iAge) * (int)bFumeur)
-  - 146.5933061 ;
-
-    p = 1 - pow(0.98767, exp(micro));
-}
-
-return p;
+return $p;
 
 ?>
--- src/Cardioscore/AccueilBundle/Controller/DefaultController.php
+++ src/Cardioscore/AccueilBundle/Controller/DefaultController.php
@@ -20,6 +20,129 @@ class DefaultController extends Controller
      */
     public function ajaxCalc($aUserDatas, $aAnalysisDatas)
     {
-        return new JsonResponse(array('data' => json_encode(['test','test'])));
+        define('FACTEUR_RISQUE_OMEGANEMIE',0.33); //modifier la valeur du milieu
+        define('FACTEUR_RISQUE_UBIOTE',0.33);//modifier la valeur du milieu
+
+        define('MAX_UBIOTEFB', 5);
+        define('MAX_UBIOTEDIVERSITE', 5);
+        define('MAX_UBIOTERICHESSE', 5);
+
+
+        /*ces 3 variables forment l'omeganemie
+        si ces trois valeurs sont bonnes l'omeganemie est égale à 0
+        si une seule de ces valeurs est mauvaise l'omeganemie est égale à 1
+        */
+        $RatioO6O3 = $_POST['ratio_o6o3'];//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true 
+        $IndexO3 = $_POST['index_o3'];//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
+        $AA_EPA = $_POST['aa_fpa'];//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
+        $iOmeganemie = 0;
+
+        //test pour savoir si l'on prend en compte le facteur d'omeganemie dans le calcul
+        if($RatioO6O3 >= 5)
+        {
+            $iOmeganemie = 1;
+        }
+        if($IndexO3 < 6 || $IndexO3 > 8)
+        {
+            $iOmeganemie = 1;
+        }
+        if($AA_EPA > 15)
+        {
+            $iOmeganemie = 1;
+        }
+
+        /*ces 3 variables forment les Ubiotes
+        si ces trois valeurs sont bonnes les Ubiotes est égale à 0
+        si une seule de ces valeurs est mauvaise les Ubiotes est égale à 1
+        */
+        $UbioteFB = $_POST['ubiote_fb'];//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
+        $UbiotteDiversite = $_POST['ubiote_diversite'];//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
+        $UbioteRichesse = $_POST['ubiote_richesse'];//a recup dans le formulaire et si la valeur est comprise dans l'intervalle donnée true
+        $iUbiotes = 0;
+
+        //test pour savoir si l'on prend en compte le facteur d'omeganemie dans le calcul
+        if($UbioteFB > MAX_UBIOTEFB) //info à recup
+        {
+            $iUbiotes = 1;
+        }
+        if($UbiotteDiversite > MAX_UBIOTEDIVERSITE) // info à recup
+        {
+            $iUbiotes = 1;
+        }
+        if(UbioteRichesse > MAX_UBIOTERICHESSE) // info à recup
+        {
+            $iUbiotes = 1;
+        }
+
+        //Ration Tout de taille/taille
+        $iHeight = $_POST['height'];
+        $iWaist = $_POST['waist'];
+        $fRatioWaistHeight = $iWaist/$iHeight;
+
+        $bSex = $_POST['sex'];
+        $bTreated = $_POST['treated'];
+        $bSmoker = $_POST['smoker'];
+        $fTotalChol $_POST['total_cholesterol'];
+        $fHDLChol = $_POST['hdl_cholesterol'];
+        $fSystolicBp = $_POST['systolic_bp'];
+
+        $iAge = $_POST['age'];
+        $iHeight = $_POST['height'];
+        $iWaist = $_POST['waist']
+
+
+        if($bSex)
+        {
+            $fMicro = 52.0096 * log($iAge) + 20.0141 * log($fTotalChol) 
+            - 0.906 * log($fHDLChol) + 1.305784 * log($fSystolicBp)
+            + 0.241549 * (int)$bTreated + 12.096316 * (int)$bSmoker 
+            - (4.605038 * log($iAge) * log($fTotalChol)) -(2.84367 * log($iAge) * (int)$bSmoker)
+            - (2.93323 * pow($iAge, 2)) - 172.300168 + (int)$iOmeganemie * FACTEUR_RISQUE_OMEGANEMIE
+            + (int)$iUbiotes * FACTEUR_RISQUE_UBIOTE + $fRatioWaistHeight;
+
+            $p = 1 - pow(0.9402, exp($fMicro));
+        }
+        else
+        {
+            $fMicro = 31.764 * log($iAge) + 22.4652 * log($fTotalChol) 
+            - 1.1877 * log($fHDLChol) + 2.5529 * log($fSystolicBp)
+            + 0.420251 * (int)$bTreated + 13.07543 * (int)$bSmoker 
+            - (5.060998 * log($iAge) * log($fTotalChol)) 
+            - (2.996945 * log($iAge) * (int)$bSmoker) - 146.5933061 
+            + (int)$iOmeganemie * FACTEUR_RISQUE_OMEGANEMIE + (int)$iUbiotes * FACTEUR_RISQUE_UBIOTE
+            + $fRatioWaistHeight;
+
+            $p = 1 - pow(0.98767, exp($fMicro));
+        }
+
+        $sMessage = 'Risque ';
+
+        if ($p > 0.4)
+        {
+            $sMessage .= 'Très élevé.';
+        }
+        elseif ($p > 0.2 && $p <= 0.4)
+        {
+            $sMessage .= 'Elevé.';
+        }
+        elseif ($p > 0.1 && $p <= 0.2)
+        {
+            $sMessage .= 'Moyen.';
+        }
+        elseif ($p > 0.05 && $p <= 0.05)
+        {
+            $sMessage .= 'Faible.';
+        }
+        else 
+        {
+            $sMessage .= 'Très faible.';
+        }
+
+        return new JsonResponse(array('data' => json_encode(
+            [
+                'fPercent' => $p * 100,
+                'sMessage' => $sMessage
+            ]
+        )));
     }
 }
